08b74935855413928a47208d7d28fe02338c98a3
rradu152 <radu.radulesc@romsym.ro>
2021-04-27 14:37:42 +0300

    fix error

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-echo "<p>test initial</p>"
-echo "<p>version 1.1</p>"
-echo "<p><a href test.php>NEXT</a></p>"
+echo "<p>test initial</p>";
+echo "<p>version 1.1</p>";
+echo "<p><a href test.php>NEXT</a></p>";
 ?>
 
