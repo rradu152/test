71ffa1f04319d49298d51e2b720135c08b759617
rradu152 <radu.radulesc@romsym.ro>
2021-04-28 11:03:52 +0300

    add localIP variable

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+$command="/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
+$localIP = exec ($command);
 echo "<p>test initial</p>";
 echo "<p>version 1.1</p>";
-echo "test";
-echo "latest";
+echo "<p>Host $localIP</p>";
 echo "<p><a href=./test.php>NEXT</a></p>";
 ?>
