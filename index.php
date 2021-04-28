56e51182c6acff02e1c0fc9e3fcc7e59a3ff0dce
rradu152 <radu.radulesc@romsym.ro>
2021-04-28 11:11:27 +0300

    add hostname

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-$command="/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
-$localIP = exec ($command);
+$localIP = exec (hostname);
 echo "<p>test initial</p>";
 echo "<p>version 1.1</p>";
 echo "<p>Host $localIP</p>";
