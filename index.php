a1e2540dedef9ce24a26d308213cf3e8dd37162b
rradu152 <radu.radulescu@gmail.com>
2021-05-18 10:24:44 +0300

    fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$localIP = exec (hostname);
+exec (hostname, $output);
 echo "<p>test initial</p>";
 echo "<p>version 1.1</p>";
-echo "<p>Host $localIP</p>";
+echo "<p>Host $output</p>";
 echo "<p><a href=./test.php>NEXT</a></p>";
 ?>
