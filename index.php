c45662adc173ba9cac4489debe42d4db1db8aca4
rradu152 <radu.radulescu@gmail.com>
2021-05-18 12:07:57 +0300

    new

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-exec (hostname, $output);
+$localIP = exec (hostname);
 echo "<p>test initial</p>";
 echo "<p>version 1.1</p>";
-echo "<p>Host $output</p>";
+echo "<p>Host $localIP</p>";
 echo "<p><a href=./test.php>NEXT</a></p>";
 ?>
